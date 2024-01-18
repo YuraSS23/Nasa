b08803b6b14afb3a195fedffbcdbd3b574304a68
YuraSS23 <nikitsenko_yura@mail.ru>
2024-01-18 16:37:37 +0300

    useeffect, preloader

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import './App.css';
 import {useEffect, useState} from "react";
 import axios from "axios";
-
+import preloader from "./assets/images/30.gif"
 function App() {
     let dayFormatter = new Intl.DateTimeFormat("en", {
         day: "2-digit",
@@ -19,16 +19,18 @@ function App() {
     const [src,setSrc] = useState([])
     const [request, setRequest] = useState(false)
     debugger
-/*    useEffect(()=>{
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=EfaW0Se22lvyshyw2NjYZrUgvxLEX8pp3mVtUmEl`)
-        .then((response)=>setSrc(response.data.hdurl))
-    },[])*/
+    useEffect(()=>{
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=EfaW0Se22lvyshyw2NjYZrUgvxLEX8pp3mVtUmEl`)
+            .then((response)=>setSrc([response.data]))
+    },[])
     const onButtonClick = ()=>{
         setRequest(true)
         axios.get(`https://api.nasa.gov/planetary/apod?api_key=EfaW0Se22lvyshyw2NjYZrUgvxLEX8pp3mVtUmEl&start_date=${startDate}&end_date=${endDate}`)
             .then((response)=>{
                 setSrc([])
-                response.data.map(el=>setSrc([...src,el.hdurl]))
+                setSrc(response.data)
+                debugger
+                //response.data.map(el=>setSrc([...src,el.hdurl]))
                 setRequest(false)
             })
     }
@@ -40,7 +42,7 @@ function App() {
           <button onClick={onButtonClick}>Get data</button>
       </div>
         <div className={"img-wrapper"}>
-            {request?<div>крутилка</div>:src.map(el=> <img src={el} width={"500px"} height={"500px"}/>)}
+            {request?<img src={preloader} height={"500px"} width={"500px"}/>:src.map((el,index)=> <div key={index}>{<img height={"500px"} width={"500px"} src={el.url} alt={"no data"}/>}</div>)}
         </div>
     </div>
   );
