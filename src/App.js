d1b5c05dac4c65f7d14abe55066993efd23383f5
YuraSS23 <nikitsenko_yura@mail.ru>
2024-01-18 15:22:23 +0300

    state, api request

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
+import React from "react";
 import './App.css';
 import {useEffect, useState} from "react";
 import axios from "axios";
 
 function App() {
-  const [src,setSrc] = useState("")
-/*  useEffect(()=>{
-    axios.get("https://api.nasa.gov/planetary/apod?api_key=EfaW0Se22lvyshyw2NjYZrUgvxLEX8pp3mVtUmEl")
-        .then((data)=>setSrc(data.data.hdurl))
-  },[])*/
+    let dayFormatter = new Intl.DateTimeFormat("en", {
+        day: "2-digit",
+    })
+    let monthFormatter = new Intl.DateTimeFormat("en", {
+        month: "2-digit",
+    })
+    let yearFormatter = new Intl.DateTimeFormat("en", {
+        year: "numeric"
+    })
+    const date = `${yearFormatter.format(new Date())}-${monthFormatter.format(new Date())}-${dayFormatter.format(new Date())}`
+    const [startDate,setStartDate] = useState(date)
+    const [endDate,setEndDate] = useState(date)
+    const [src,setSrc] = useState([])
+    const [request, setRequest] = useState(false)
+    debugger
+/*    useEffect(()=>{
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=EfaW0Se22lvyshyw2NjYZrUgvxLEX8pp3mVtUmEl`)
+        .then((response)=>setSrc(response.data.hdurl))
+    },[])*/
+    const onButtonClick = ()=>{
+        setRequest(true)
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=EfaW0Se22lvyshyw2NjYZrUgvxLEX8pp3mVtUmEl&start_date=${startDate}&end_date=${endDate}`)
+            .then((response)=>{
+                setSrc([])
+                response.data.map(el=>setSrc([...src,el.hdurl]))
+                setRequest(false)
+            })
+    }
   return (
     <div className="App">
       <div className={"date-wrapper"}>
-          <input type={"date"}/>
-          <input type={"date"}/>
+          <input value={startDate} onChange={(e)=>setStartDate(e.currentTarget.value)} type={"date"}/>
+          <input value={endDate} onChange={(e)=>setEndDate(e.currentTarget.value)} type={"date"}/>
+          <button onClick={onButtonClick}>Get data</button>
       </div>
         <div className={"img-wrapper"}>
-            <img src={src} width={"500px"} height={"500px"}/>
+            {request?<div>крутилка</div>:src.map(el=> <img src={el} width={"500px"} height={"500px"}/>)}
         </div>
     </div>
   );
