bc0ff315b91eb3763774e0d98d9c191279d0877a
YuraSS23 <nikitsenko_yura@mail.ru>
2024-01-19 11:23:10 +0300

    current date, container? wrapper

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import {useEffect, useState} from "react";
 import axios from "axios";
 import preloader from "./assets/images/30.gif"
+import {Container} from "./components/Container";
+import {FlexWrapper} from "./components/Flex-wrapper";
+
 function App() {
     let dayFormatter = new Intl.DateTimeFormat("en", {
         day: "2-digit",
@@ -14,38 +17,56 @@ function App() {
         year: "numeric"
     })
     const date = `${yearFormatter.format(new Date())}-${monthFormatter.format(new Date())}-${dayFormatter.format(new Date())}`
-    const [startDate,setStartDate] = useState(date)
-    const [endDate,setEndDate] = useState(date)
-    const [src,setSrc] = useState([])
+    const [startDate, setStartDate] = useState(date)
+    const [endDate, setEndDate] = useState(date)
+    const [currentDate, setCurrentDate] = useState(date)
+    const [src, setSrc] = useState([])
     const [request, setRequest] = useState(false)
     debugger
-    useEffect(()=>{
+    useEffect(() => {
         axios.get(`https://api.nasa.gov/planetary/apod?api_key=EfaW0Se22lvyshyw2NjYZrUgvxLEX8pp3mVtUmEl`)
-            .then((response)=>setSrc([response.data]))
-    },[])
-    const onButtonClick = ()=>{
+            .then((response) => setSrc([response.data]))
+    }, [])
+    const onButtonClick = () => {
         setRequest(true)
         axios.get(`https://api.nasa.gov/planetary/apod?api_key=EfaW0Se22lvyshyw2NjYZrUgvxLEX8pp3mVtUmEl&start_date=${startDate}&end_date=${endDate}`)
-            .then((response)=>{
+            .then((response) => {
                 setSrc([])
                 setSrc(response.data)
-                debugger
-                //response.data.map(el=>setSrc([...src,el.hdurl]))
                 setRequest(false)
             })
     }
-  return (
-    <div className="App">
-      <div className={"date-wrapper"}>
-          <input value={startDate} onChange={(e)=>setStartDate(e.currentTarget.value)} type={"date"}/>
-          <input value={endDate} onChange={(e)=>setEndDate(e.currentTarget.value)} type={"date"}/>
-          <button onClick={onButtonClick}>Get data</button>
-      </div>
-        <div className={"img-wrapper"}>
-            {request?<img src={preloader} height={"500px"} width={"500px"}/>:src.map((el,index)=> <div key={index}>{<img height={"500px"} width={"500px"} src={el.url} alt={"no data"}/>}</div>)}
-        </div>
-    </div>
-  );
+    const onButton2Click = () => {
+        setRequest(true)
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=EfaW0Se22lvyshyw2NjYZrUgvxLEX8pp3mVtUmEl&start_date=${currentDate}&end_date=${currentDate}`)
+            .then((response) => {
+                setSrc([])
+                setSrc(response.data)
+                setRequest(false)
+            })
+    }
+    return (
+        <Container>
+            <div className="App">
+                <h1>NASA picture of the day</h1>
+                <div className={"date-wrapper"}>
+                    <span>Input date</span>
+                    <input value={startDate} onChange={(e) => setStartDate(e.currentTarget.value)} type={"date"}/>
+                    <input value={endDate} onChange={(e) => setEndDate(e.currentTarget.value)} type={"date"}/>
+                    <button onClick={onButtonClick}>Get data</button>
+                </div>
+                <div className={"date-wrapper"}>
+                    <span>Input current date</span>
+                    <input value={currentDate} onChange={(e) => setCurrentDate(e.currentTarget.value)} type={"date"}/>
+                    <button onClick={onButton2Click}>Get data</button>
+                </div>
+                <FlexWrapper wrap={"wrap"} justify={"space-around"} gap={"20px"}>
+                    {request ? <img src={preloader} height={"500px"} width={"500px"} alt={"preloader"} className={"nasa-img"}/>
+                        : src.map((el, index) => <img key={index} height={"500px"} width={"500px"} src={el.url} alt={"no data"}/>)}
+                </FlexWrapper>
+                </div>
+        </Container>
+    );
 }
 
 export default App;
---
+++ src/components/Flex-wrapper.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+
+export const FlexWrapper = styled.div`
+  display: flex;
+  flex-direction: ${props => props.direction || "row"};
+  justify-content: ${props => props.justify || "flex-start"};
+  align-items: ${props => props.align || "stretch"};
+  flex-wrap: ${props => props.wrap || "nowrap"};
+  height: 100%;
+  gap: ${props => props.gap || "0"};;
+`
